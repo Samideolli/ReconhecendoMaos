7ce4bc117c28ab7f840b82e341be69d5cf7bd49d
Samara de Oliveira <99887045+Samideolli@users.noreply.github.com>
2023-11-28 14:58:20 -0300

    Update count_fingers.py

--- count_fingers.py
+++ count_fingers.py
@@ -10,24 +10,24 @@ hands = mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5
 
 tipIds = [4, 8, 12, 16, 20]
 
-# Defina uma função para contar os dedos
+
 
 def countFingers(image, hand_landmarks, handNo=0):
     
     if hand_landmarks:
-        # Obtenha todos os pontos de referência da PRIMEIRA mão VISÍVEL
+       
         landmarks = hand_landmarks[handNo].landmark
         # print(landmarks)
 
-        # Conte os dedos
+       
         fingers = []
 
         for lm_index in tipIds:
-                # Obtenha os valores y da ponta e da parte inferior do dedo
+                
                 finger_tip_y = landmarks[lm_index].y 
                 finger_bottom_y = landmarks[lm_index - 2].y
 
-                # Verifique se ALGUM DEDO está ABERTO ou FECHADO
+              
                 if lm_index !=4:
                     if finger_tip_y < finger_bottom_y:
                         fingers.append(1)
@@ -40,15 +40,15 @@ def countFingers(image, hand_landmarks, handNo=0):
         # print(fingers)
         totalFingers = fingers.count(1)
 
-        # Exiba o texto
+      
         text = f'Dedos: {totalFingers}'
 
         cv2.putText(image, text, (50,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0,0), 2)
 
-# Defina uma função para 
+
 def drawHandLanmarks(image, hand_landmarks):
 
-    # Desenhar as conexões entre os pontos de referência
+    
     if hand_landmarks:
 
       for landmarks in hand_landmarks:
@@ -61,21 +61,21 @@ while True:
 
     image = cv2.flip(image, 1)
     
-    # Detecte os pontos de referência das mãos 
+   
     results = hands.process(image)
 
-    # Obtenha a posição do ponto de referência do resultado processado
+    
     hand_landmarks = results.multi_hand_landmarks
 
-    # Desenhe os pontos de referência
+    
     drawHandLanmarks(image, hand_landmarks)
 
-    # Obtenha a posição dos dedos da mão        
+        
     countFingers(image, hand_landmarks)
 
     cv2.imshow("Controlador de Midia", image)
 
-    # Saia da tela ao pressionar a barra de espaços
+    
     key = cv2.waitKey(1)
     if key == 32:
         break
